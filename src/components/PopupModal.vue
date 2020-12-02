12c095437d388c038c3faada20ac029adb0b2530
kirtap5 <patrik.gerdelius@hotmail.com>
2020-12-02 08:36:15 +0100

    displaying works

---
+++ src/components/PopupModal.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- <div v-if="card.x>0" :class="['card', {'available-to-choose': availableAction}]" :style="{'background-position': (-(card.x-1)*250)+'px ' + (-(card.y-1)*350)+'px'}" @click="doAction"> -->
+    <div v-on:click="showModal">CLICK ME</div>
+    <div v-if="isVisible">WOOOOOOO</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PopupModal",
+  props: {},
+  data: function () {
+    return{
+    isVisible: false,
+    }
+  },
+  methods: {
+    showModal: function () {
+      this.isVisible == false ? this.isVisible = true : this.isVisible = false;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
